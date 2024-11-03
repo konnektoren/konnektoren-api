b34a22543cb158d9fcace1811443abef9961e34f
Christian M <chriamue@gmail.com>
2024-11-03 11:05:58 +0100

    :bug: fix add performance record

--- src/services/v1/leaderboard.rs
+++ src/services/v1/leaderboard.rs
@@ -1,6 +1,5 @@
 use crate::storage::{RepositoryError, Storage};
-use chrono::Duration;
-use konnektoren_core::challenges::{PerformanceRecord, Timed};
+use konnektoren_core::challenges::PerformanceRecord;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -11,40 +10,39 @@ pub async fn add_performance_record(
     performance_record: PerformanceRecord,
     repository: Arc<Mutex<dyn Storage>>,
 ) -> Result<PerformanceRecord, RepositoryError> {
-    let mut leaderboard = repository
-        .lock()
+    let mut storage = repository.lock().await;
+
+    // First try to add directly
+    match storage
+        .add_performance_record(namespace, performance_record.clone())
         .await
-        .fetch_performance_records(namespace)
-        .await?;
+    {
+        Ok(record) => return Ok(record),
+        Err(RepositoryError::LimitReached(_)) => {
+            // If limit reached, get all records to compare
+            let mut leaderboard = storage.fetch_performance_records(namespace).await?;
+            leaderboard.sort();
 
-    if leaderboard.len() >= PERFORMANCE_RECORDS_LIMIT {
-        // remove record with the lowest performance
-        leaderboard.sort();
+            // If there are records and the new record is better than the worst one
+            if let Some(worst_record) = leaderboard.last() {
+                if worst_record > &performance_record {
+                    // Remove the worst record
+                    storage
+                        .remove_performance_record(namespace, worst_record.clone())
+                        .await?;
 
-        let record_to_remove = leaderboard.last().unwrap();
+                    // Try adding the new record again
+                    storage
+                        .add_performance_record(namespace, performance_record.clone())
+                        .await?;
 
-        if record_to_remove.performance_percentage <= performance_record.performance_percentage {
-            repository
-                .lock()
-                .await
-                .remove_performance_record(namespace, record_to_remove.clone())
-                .await?;
-            repository
-                .lock()
-                .await
-                .add_performance_record(namespace, performance_record.clone())
-                .await?;
-            return Ok(performance_record);
+                    return Ok(performance_record);
+                }
+            }
+            Err(RepositoryError::LimitReached(PERFORMANCE_RECORDS_LIMIT))
         }
-    } else {
-        repository
-            .lock()
-            .await
-            .add_performance_record(namespace, performance_record.clone())
-            .await?;
+        Err(e) => Err(e),
     }
-
-    Ok(performance_record)
 }
 
 pub async fn fetch_all_performance_records(
@@ -62,7 +60,54 @@ pub async fn fetch_all_performance_records(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::storage::{LeaderboardRepository, MemoryRepository};
     use chrono::DateTime;
+
+    #[test]
+    fn test_sort_performance() {
+        let a = PerformanceRecord {
+            game_path_id: "".to_string(),
+            profile_name: "a".to_string(),
+            challenges_performance: vec![("".to_string(), 100, 100)],
+            date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-01T00:00:00Z").unwrap()),
+            performance_percentage: 100,
+            total_challenges: 0,
+        };
+
+        let b = PerformanceRecord {
+            game_path_id: "".to_string(),
+            profile_name: "b".to_string(),
+            challenges_performance: vec![("".to_string(), 100, 400)],
+            date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-01T00:00:00Z").unwrap()),
+            performance_percentage: 100,
+            total_challenges: 0,
+        };
+
+        let c = PerformanceRecord {
+            game_path_id: "".to_string(),
+            profile_name: "c".to_string(),
+            challenges_performance: vec![("".to_string(), 100, 300)],
+            date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-02T00:00:00Z").unwrap()),
+            performance_percentage: 100,
+            total_challenges: 0,
+        };
+
+        let d = PerformanceRecord {
+            game_path_id: "".to_string(),
+            profile_name: "d".to_string(),
+            challenges_performance: vec![("".to_string(), 100, 200)],
+            date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-02T00:00:00Z").unwrap()),
+            performance_percentage: 100,
+            total_challenges: 0,
+        };
+
+        let mut leaderboard = vec![a.clone(), b.clone(), c.clone(), d.clone()];
+        leaderboard.sort();
+
+        assert_eq!(leaderboard, vec![a, d, c, b.clone()]);
+        assert_eq!(leaderboard.last().unwrap(), &b);
+    }
+
     #[test]
     fn test_sort_performance_and_date() {
         let a = PerformanceRecord {
@@ -101,14 +146,74 @@ mod tests {
             total_challenges: 0,
         };
 
-        assert_eq!(a.cmp(&b), std::cmp::Ordering::Less);
-        assert_eq!(a.cmp(&c), std::cmp::Ordering::Greater);
-        assert_eq!(a.cmp(&d), std::cmp::Ordering::Less);
-        assert_eq!(b.cmp(&c), std::cmp::Ordering::Greater);
-        assert_eq!(b.cmp(&d), std::cmp::Ordering::Greater);
-
         let mut vec = vec![a.clone(), b.clone(), c.clone(), d.clone()];
         vec.sort();
         assert_eq!(vec, vec![c, a, d, b]);
+    }
+
+    #[tokio::test]
+    async fn test_add_limit_reached() {
+        let storage = MemoryRepository::new();
+        let repository = Arc::new(Mutex::new(storage));
+
+        let namespace = "test";
+        let mut records = vec![];
+
+        // Fill up the leaderboard
+        for i in 0..PERFORMANCE_RECORDS_LIMIT {
+            let record = PerformanceRecord {
+                profile_name: i.to_string(),
+                challenges_performance: vec![("".to_string(), 100, 200)],
+                date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-01T00:00:00Z").unwrap()),
+                performance_percentage: 100,
+                total_challenges: 1,
+                ..Default::default()
+            };
+            records.push(record.clone());
+
+            // Use repository.lock() to access storage methods
+            repository
+                .lock()
+                .await
+                .add_performance_record(namespace, record)
+                .await
+                .unwrap();
+        }
+
+        let new_record = PerformanceRecord {
+            profile_name: "new".to_string(),
+            challenges_performance: vec![("".to_string(), 100, 100)],
+            date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-01T00:00:00Z").unwrap()),
+            performance_percentage: 100,
+            total_challenges: 1,
+            ..Default::default()
+        };
+
+        let result = add_performance_record(namespace, new_record, repository.clone()).await;
+        assert!(result.is_ok());
+
+        let new_worse_record = PerformanceRecord {
+            profile_name: "new_worse".to_string(),
+            challenges_performance: vec![("".to_string(), 100, 500)],
+            date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-01T00:00:00Z").unwrap()),
+            performance_percentage: 0,
+            total_challenges: 1,
+            ..Default::default()
+        };
+
+        let result = add_performance_record(namespace, new_worse_record, repository.clone()).await;
+        assert!(matches!(result, Err(RepositoryError::LimitReached(_))));
+
+        let new_best_record = PerformanceRecord {
+            profile_name: "new_best".to_string(),
+            challenges_performance: vec![("".to_string(), 100, 10)],
+            date: DateTime::from(DateTime::parse_from_rfc3339("2021-08-01T00:00:00Z").unwrap()),
+            performance_percentage: 100,
+            total_challenges: 1,
+            ..Default::default()
+        };
+
+        let result = add_performance_record(namespace, new_best_record, repository).await;
+        assert!(result.is_ok());
     }
 }
